143aca1db46d613f1b580c8de40d61622ae2d55e
Eduardoamaral66 <eduardoamaral66@gmail.com>
2020-04-25 14:45:44 -0300

    feat: add onFocus and onFilled styles

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,14 +1,41 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
   padding: 16px;
   width: 100%;
-  color: #666360;
   display: flex;
   align-items: center;
+
+  border: 2px solid #232129;
+  color: #666360;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      svg {
+        color: #ff9000 !important;
+      }
+      border-color: #ff9000;
+    `}
+
+  ${(props) =>
+    props.isFilled &&
+    css`
+      svg {
+        color: #ff9000 !important;
+      }
+    `}
 
   svg {
     margin-right: 16px;
@@ -24,8 +51,5 @@ export const Container = styled.div`
     &::placeholder {
       color: #666360;
     }
-  }
-  & + div {
-    margin-top: 8px;
   }
 `;
