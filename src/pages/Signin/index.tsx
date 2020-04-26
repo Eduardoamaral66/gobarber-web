652b0a99c14127ebbab34e4eedf82d18131fcc12
Eduardoamaral66 <eduardoamaral66@gmail.com>
2020-04-26 13:32:59 -0300

    feat: add signOut

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -9,7 +9,7 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 import getValidationErrors from '../../utils/getValidationErrors';
 import { signInSchema } from '../../utils/yup.schemas';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 
 interface SiginFormData {
   email: string;
