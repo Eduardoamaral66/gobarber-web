2c1a11fa5132b33f8a4bc73ce28202797b75527d
Eduardoamaral66 <eduardoamaral66@gmail.com>
2020-05-31 16:49:30 -0300

    feat: add nextAppointment element

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,69 @@
 import React from 'react';
+import { FiPower, FiClock } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo, </span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars0.githubusercontent.com/u/5282443?v=4"
+                alt="teste"
+              />
+              <strong>Diego Fernandes</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
+    </Container>
+  );
+};
 
 export default Dashboard;
