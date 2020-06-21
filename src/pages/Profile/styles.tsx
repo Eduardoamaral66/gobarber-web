5db74947d2704a5d9313bccbfac82bbea908420e
Eduardoamaral66 <eduardoamaral66@gmail.com>
2020-06-21 10:41:25 -0300

    feat: update profile avatar

--- src/pages/Profile/styles.tsx
+++ src/pages/Profile/styles.tsx
@@ -68,7 +68,7 @@ export const AvatarInput = styled.div`
     border-radius: 50%;
   }
 
-  button {
+  label {
     position: absolute;
     width: 48px;
     height: 48px;
@@ -77,18 +77,25 @@ export const AvatarInput = styled.div`
     right: 0;
     bottom: 0;
     border: 0;
+    cursor: pointer;
     transition: background-color 0.2s;
 
     display: flex;
     align-items: center;
     justify-content: center;
+
     &:hover {
       background: ${shade(0.2, '#ff9000')};
+    }
+
+    input {
+      display: none;
     }
 
     svg {
       width: 20px;
       height: 20px;
+      color: #312e38;
     }
   }
 `;
